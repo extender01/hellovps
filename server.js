08ff4e84fc52588439992fb95ca4d30c122b93d0
Vit Krajicek <vit@krajicek.org>
2019-03-22 20:23:58 +0100

    9

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Ahoj tady IMPULSOV zkousime remote push z VPS nebo ne ze by ndefdd'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!. NODE_ENV je ${NODE_ENV}`))
